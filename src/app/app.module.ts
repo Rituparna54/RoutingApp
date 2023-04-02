b6588e863996423b1b658e011f13b29b4ee7150d
Rituparna Saha <rituparnasaha54@gmail.com>
2023-04-02 14:25:35 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrderComponent } from './order/order.component';
+import { OrderComponentComponent } from './order-component/order-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderComponent,
+    OrderComponentComponent
   ],
   imports: [
     BrowserModule,
